dfadb2facbedad66de7ca48095131c4574428bb0
jadanmorrow <jadanmorrow@outlook.com>
2022-12-28 15:11:18 -0700

    Add color to paddles and ball

--- pong_game.py
+++ pong_game.py
@@ -15,7 +15,7 @@ score_b = 0
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("red")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -24,7 +24,7 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("blue")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -33,7 +33,7 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("white")
+ball.color("lime")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 1.5
@@ -52,22 +52,31 @@ pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "norm
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
+    if y > 250:  # check if the paddle is at the top border
+        y = 250  # set the paddle's y-coordinate to the top border
     paddle_a.sety(y)
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
+    if y < -250:  # check if the paddle is at the bottom border
+        y = -250  # set the paddle's y-coordinate to the bottom border
     paddle_a.sety(y)
 
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
+    if y > 250:  # check if the paddle is at the top border
+        y = 250  # set the paddle's y-coordinate to the top border
     paddle_b.sety(y)
 
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
+    if y < -250:  # check if the paddle is at the bottom border
+        y = -250  # set the paddle's y-coordinate to the bottom border
     paddle_b.sety(y)
+
 
 # Keyboard bindings
 wn.listen()
